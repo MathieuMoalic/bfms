8957b8f934c748efdefd41c1be06272275901ea9
Mathieu Moalic <matmoa@pm.me>
2022-10-24 12:40:49 +0200

    Add magnetization attribute

--- src/lib.rs
+++ src/lib.rs
@@ -1,199 +1,42 @@
-use std::ops::Add;
-use std::sync::Arc;
-
-use numpy::ndarray::{Array1, ArrayD, ArrayView1, ArrayViewD, ArrayViewMutD, Zip};
-use numpy::{
-    datetime::{units, Timedelta},
-    Complex64, IntoPyArray, PyArray1, PyArrayDyn, PyReadonlyArray1, PyReadonlyArrayDyn,
-    PyReadwriteArray1, PyReadwriteArrayDyn,
-};
+use numpy::PyArray4;
 use pyo3::prelude::*;
-use pyo3::types::{PyDict, PyTuple};
-use pyo3::{
-    exceptions::PyIndexError, pymodule, types::PyModule, FromPyObject, PyAny, PyObject, PyResult,
-    Python,
-};
+use pyo3::{pymodule, types::PyModule, PyResult, Python};
 
 #[pyclass]
 struct Sim {
     #[pyo3(get, set)]
-    num: i32,
-    #[pyo3(get, set)]
     name: String,
     #[pyo3(get, set)]
-    m: Arc<Option<PyArray1<f64>>>,
+    m: Py<PyArray4<f64>>,
+    #[pyo3(get)]
+    t: f64,
 }
 
 #[pymethods]
 impl Sim {
     #[new]
-    #[args(num = "-1")]
-    fn new(num: i32, name: String) -> Self {
-        Sim {
-            num,
-            name,
-            m: todo!(),
-        }
+    fn new(py: Python<'_>, name: String) -> Self {
+        let m = PyArray4::zeros(py, [1, 1, 1, 3], true).into_py(py);
+
+        let t = 0.;
+        Sim { name, m, t }
     }
 
-    fn __call__(&self) -> String {
-        format!("{:?}", self.num)
+    fn run(&self, py: Python<'_>, t: f64) {
+        let mut m = self.m.as_ref(py).readwrite();
+        let mut m = m.as_array_mut();
+        m *= t
     }
 
-    #[args(num = "10", py_args = "*", name = "\"Hello\"", py_kwargs = "**")]
-    fn method(
-        &mut self,
-        num: i32,
-        name: &str,
-        py_args: &PyTuple,
-        py_kwargs: Option<&PyDict>,
-    ) -> String {
-        let num_before = self.num;
-        self.num = num;
-        format!(
-            "py_args={:?}, py_kwargs={:?}, name={}, num={} num_before={}",
-            py_args, py_kwargs, name, self.num, num_before,
-        )
+    fn double_array(&self, py: Python<'_>) {
+        let mut m = self.m.as_ref(py).readwrite();
+        let mut m = m.as_array_mut();
+        m *= 2.0;
     }
-    // #[args(m)]
 }
 
 #[pymodule]
 fn gwarell(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
     m.add_class::<Sim>()?;
-    // example using generic PyObject
-    fn head(x: ArrayViewD<'_, PyObject>) -> PyResult<PyObject> {
-        x.get(0)
-            .cloned()
-            .ok_or_else(|| PyIndexError::new_err("array index out of range"))
-    }
-
-    // example using immutable borrows producing a new array
-    fn axpy(a: f64, x: ArrayViewD<'_, f64>, y: ArrayViewD<'_, f64>) -> ArrayD<f64> {
-        a * &x + &y
-    }
-
-    // example using a mutable borrow to modify an array in-place
-    fn mult(a: f64, mut x: ArrayViewMutD<'_, f64>) {
-        x *= a;
-    }
-
-    // example using complex numbers
-    fn conj(x: ArrayViewD<'_, Complex64>) -> ArrayD<Complex64> {
-        x.map(|c| c.conj())
-    }
-
-    // example using generics
-    fn generic_add<T: Copy + Add<Output = T>>(x: ArrayView1<T>, y: ArrayView1<T>) -> Array1<T> {
-        &x + &y
-    }
-
-    // wrapper of `head`
-    #[pyfn(m)]
-    #[pyo3(name = "head")]
-    fn head_py(_py: Python<'_>, x: PyReadonlyArrayDyn<'_, PyObject>) -> PyResult<PyObject> {
-        head(x.as_array())
-    }
-
-    // wrapper of `axpy`
-    #[pyfn(m)]
-    #[pyo3(name = "axpy")]
-    fn axpy_py<'py>(
-        py: Python<'py>,
-        a: f64,
-        x: PyReadonlyArrayDyn<'_, f64>,
-        y: PyReadonlyArrayDyn<'_, f64>,
-    ) -> &'py PyArrayDyn<f64> {
-        let x = x.as_array();
-        let y = y.as_array();
-        let z = axpy(a, x, y);
-        z.into_pyarray(py)
-    }
-
-    // wrapper of `mult`
-    #[pyfn(m)]
-    #[pyo3(name = "mult")]
-    fn mult_py(a: f64, mut x: PyReadwriteArrayDyn<f64>) {
-        let x = x.as_array_mut();
-        mult(a, x);
-    }
-
-    // wrapper of `conj`
-    #[pyfn(m)]
-    #[pyo3(name = "conj")]
-    #[pyo3(text_signature = "(py,x,/)")]
-    fn conj_py<'py>(
-        py: Python<'py>,
-        x: PyReadonlyArrayDyn<'_, Complex64>,
-    ) -> &'py PyArrayDyn<Complex64> {
-        conj(x.as_array()).into_pyarray(py)
-    }
-
-    // example of how to extract an array from a dictionary
-    #[pyfn(m)]
-    fn extract(d: &PyDict) -> f64 {
-        let x = d
-            .get_item("x")
-            .unwrap()
-            .downcast::<PyArray1<f64>>()
-            .unwrap();
-
-        x.readonly().as_array().sum()
-    }
-
-    // example using timedelta64 array
-    #[pyfn(m)]
-    fn add_minutes_to_seconds(
-        mut x: PyReadwriteArray1<Timedelta<units::Seconds>>,
-        y: PyReadonlyArray1<Timedelta<units::Minutes>>,
-    ) {
-        #[allow(deprecated)]
-        Zip::from(x.as_array_mut())
-            .and(y.as_array())
-            .apply(|x, y| *x = (i64::from(*x) + 60 * i64::from(*y)).into());
-    }
-
-    // This crate follows a strongly-typed approach to wrapping NumPy arrays
-    // while Python API are often expected to work with multiple element types.
-    //
-    // That kind of limited polymorphis can be recovered by accepting an enumerated type
-    // covering the supported element types and dispatching into a generic implementation.
-    #[derive(FromPyObject)]
-    enum SupportedArray<'py> {
-        F64(&'py PyArray1<f64>),
-        I64(&'py PyArray1<i64>),
-    }
-
-    #[pyfn(m)]
-    fn polymorphic_add<'py>(
-        x: SupportedArray<'py>,
-        y: SupportedArray<'py>,
-    ) -> PyResult<&'py PyAny> {
-        match (x, y) {
-            (SupportedArray::F64(x), SupportedArray::F64(y)) => Ok(generic_add(
-                x.readonly().as_array(),
-                y.readonly().as_array(),
-            )
-            .into_pyarray(x.py())
-            .into()),
-            (SupportedArray::I64(x), SupportedArray::I64(y)) => Ok(generic_add(
-                x.readonly().as_array(),
-                y.readonly().as_array(),
-            )
-            .into_pyarray(x.py())
-            .into()),
-            (SupportedArray::F64(x), SupportedArray::I64(y))
-            | (SupportedArray::I64(y), SupportedArray::F64(x)) => {
-                let y = y.cast::<f64>(false)?;
-
-                Ok(
-                    generic_add(x.readonly().as_array(), y.readonly().as_array())
-                        .into_pyarray(x.py())
-                        .into(),
-                )
-            }
-        }
-    }
-
     Ok(())
 }
--- tests/test_ext.py
+++ tests/test_ext.py
@@ -1,76 +1,8 @@
 import numpy as np
-from gwarell import (
-    head,
-    axpy,
-    conj,
-    mult,
-    extract,
-    add_minutes_to_seconds,
-    polymorphic_add,
-    Sim,
-)
+from gwarell import Sim
 
 
 def test_sim():
-    s = Sim(12, "hi")
-    print(s())
-    # print(s.num, s.name)
-    # print(s.method(44, False, "World", 666, x=44, y=55))
-    # print(help(conj))
-    # s.m = np.ones((3,3))
-
-
-def test_head():
-    x = np.array(["first", None, 42])
-    first = head(x)
-    assert first == "first"
-
-
-def test_axpy():
-    x = np.array([1.0, 2.0, 3.0])
-    y = np.array([3.0, 3.0, 3.0])
-    z = axpy(3.0, x, y)
-    np.testing.assert_array_almost_equal(z, np.array([6.0, 9.0, 12.0]))
-
-
-def test_mult():
-    x = np.array([1.0, 2.0, 3.0])
-    mult(3.0, x)
-    np.testing.assert_array_almost_equal(x, np.array([3.0, 6.0, 9.0]))
-
-
-def test_conj():
-    x = np.array([1.0 + 2j, 2.0 + 3j, 3.0 + 4j])
-    np.testing.assert_array_almost_equal(conj(x), np.conj(x))
-
-
-def test_extract():
-    x = np.arange(5.0)
-    d = {"x": x}
-    np.testing.assert_almost_equal(extract(d), 10.0)
-
-
-def test_add_minutes_to_seconds():
-    x = np.array([10, 20, 30], dtype="timedelta64[s]")
-    y = np.array([1, 2, 3], dtype="timedelta64[m]")
-
-    add_minutes_to_seconds(x, y)
-
-    assert np.all(x == np.array([70, 140, 210], dtype="timedelta64[s]"))
-
-
-def test_polymorphic_add():
-    x = np.array([1.0, 2.0, 3.0], dtype=np.double)
-    y = np.array([3.0, 3.0, 3.0], dtype=np.double)
-    z = polymorphic_add(x, y)
-    np.testing.assert_array_almost_equal(z, np.array([4.0, 5.0, 6.0], dtype=np.double))
-
-    x = np.array([1, 2, 3], dtype=np.int64)
-    y = np.array([3, 3, 3], dtype=np.int64)
-    z = polymorphic_add(x, y)
-    assert np.all(z == np.array([4, 5, 6], dtype=np.int64))
-
-    x = np.array([1.0, 2.0, 3.0], dtype=np.double)
-    y = np.array([3, 3, 3], dtype=np.int64)
-    z = polymorphic_add(x, y)
-    np.testing.assert_array_almost_equal(z, np.array([4.0, 5.0, 6.0], dtype=np.double))
+    s = Sim("simi")
+    s.run(3)
+    print(s.m)
